031320cbe9a3b046e3a79a7f373fc3c3461fa831
cpoe07041776 <christopher.poe@me.com>
2025-06-06 16:24:02 -0500

    feat: initialize Netlify Identity in index.js and wire up login in App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';    // ← this triggers Tailwind’s CSS build
-import App from './App';
-import netlifyIdentity from 'netlify-identity-widget';
 
-// This will automatically detect your Netlify site and enable the Identity modal
-netlifyIdentity.init();
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+// src/index.js
+import netlifyIdentity from 'netlify-identity-widget'
+import 'netlify-identity-widget/styles.css'
+
+// Initialize Netlify Identity as early as possible
+netlifyIdentity.init()
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
