25df324757acae50258ef5b3e6376a6812333d38
cpoe07041776 <christopher.poe@me.com>
2025-06-06 16:35:06 -0500

    fix: import Identity widget CSS from build folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-
-
+// src/index.js
+import netlifyIdentity from 'netlify-identity-widget'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-// src/index.js
-import netlifyIdentity from 'netlify-identity-widget'
-import 'netlify-identity-widget/styles.css'
 
 // Initialize Netlify Identity as early as possible
 netlifyIdentity.init()
