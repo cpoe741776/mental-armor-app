b0f0a02a5117a1655c8f08d065d4251f4b839793
cpoe07041776 <christopher.poe@me.com>
2025-06-08 06:22:51 -0500

    Integrate and center MFADials widget in Profile page

--- src/components/MFADials.jsx
+++ src/components/MFADials.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent } from '@/components/ui/card';
+import { Card, CardContent } from './ui/card';
 import { RadialBarChart, RadialBar } from 'recharts';
 
 // Maximum possible score (e.g., 5)
