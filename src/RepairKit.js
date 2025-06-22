ab63e1e3133b15a40728eba16647553fd9e8a85b
cpoe07041776 <christopher.poe@me.com>
2025-06-22 12:30:09 +0100

    Add Voices

--- src/RepairKit.js
+++ src/RepairKit.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import CoachArmorChat from './components/CoachArmorChat';
 import { skills } from './skills';
 
